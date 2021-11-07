d01e3c40058fa60e5393fbd29913409cfbb9c473
okabekaito <kaito19991204@gmail.com>
2021-11-07 22:10:51 +0900

    TokyoTime

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,5 @@
 class Note < ApplicationRecord
+    validates :title, length: { maximum: 50 }, presence: true
+    validates :author, length: { maximum: 15 }, presence: true
+    validates :content, length: { maximum: 1000 }, presence: true
 end
